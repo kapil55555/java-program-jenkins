2e23326e7c0de0f85091f45d9a75aeb1ff74c1ef
kapil55555 <theharshitkumar500@gmail.com>
2024-03-19 01:48:48 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+public class Main{
+    public static void main(String[] args){
+        System.out.println("this is java program");
+         System.out.println("this is output from java program");
+          System.out.println(new Date());
+    }
+}
