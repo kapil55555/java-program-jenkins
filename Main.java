6cef767ef61fdd2bfc41d65fd397d2f7addbdabd
kapil55555 <theharshitkumar500@gmail.com>
2024-03-19 02:42:41 +0530

    Added new line checking automation

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public class Main{
          System.out.println("this is output from java program");
           System.out.println(new Date());
           System.out.println("hello"); 
+          System.out.println("to check if jenkins process is automated ??"); 
     }
 }
