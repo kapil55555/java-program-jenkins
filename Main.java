abdbcda6524b3627b8fc2004f5b9a8b86cfd618b
kapil55555 <theharshitkumar500@gmail.com>
2024-03-19 01:51:11 +0530

    initial commit file

--- Main.java
+++ Main.java
@@ -4,5 +4,6 @@ public class Main{
         System.out.println("this is java program");
          System.out.println("this is output from java program");
           System.out.println(new Date());
+          System.out.println("hello"); 
     }
 }
